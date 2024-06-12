dbeabbc92b6510c414631981942c071fb61b3596
egnica <46502133+egnica@users.noreply.github.com>
2024-06-11 21:56:53 -0500

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,22 +3,52 @@ import styles from "./page.module.css";
 import Image from "next/image";
 
 export default function Home() {
-	const coverImage =
-		"https://latestartbucket.s3.us-east-2.amazonaws.com/park-tree.png";
+	const timeAsDev = () => {
+		const date = new Date();
+
+		let month = date.getMonth();
+		let year = date.getFullYear();
+
+		const startMonth = 4;
+		const startYear = 2020;
+
+		return;
+	};
 
 	return (
 		<>
 			<h1>Late Start Dev</h1>
-			<h2>test</h2>
-			<p>Testing this stufffff</p>
-			<div
-				style={{
-					position: "relative",
-					width: "500px",
-					height: "375px",
-				}}
-			>
-				<Image alt='garden tree' fill priority={true} src={coverImage} />
+
+			<div className={styles.picture_text_contain}>
+				<Image
+					style={{borderRadius: "20px"}}
+					className={styles.image_contain}
+					width={1825}
+					height={2499}
+					alt='Picture of Nicholas Egner'
+					layout='responsive'
+					priority={true}
+					src='https://latestartbucket.s3.us-east-2.amazonaws.com/nick-pic.png'
+				/>
+				<div className={styles.text_button_contain}>
+					<h2>Welcome to my blog</h2>
+					<br />
+					<p>
+						Hello! My name is
+						<a href='https://www.linkedin.com/in/nicholas-egner/'>
+							Nicholas Egner
+						</a>
+						. I have been a profesional web developer for {timeAsDev()}
+						<br />
+						<br />
+						<br />
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</p>
+					<button>TEST</button>
+				</div>
 			</div>
 		</>
 	);
