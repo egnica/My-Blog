bb02032b26718f491685d688ffe978d771b58628
egnica <46502133+egnica@users.noreply.github.com>
2024-06-17 17:00:16 -0500

    added to display page

--- src/app/display_blog/page.jsx
+++ src/app/display_blog/page.jsx
@@ -16,12 +16,13 @@ const Display = () => {
 			title: Posts.posts[lastIndex - 1].title,
 			description: Posts.posts[lastIndex - 1].description,
 			articleImage: Posts.posts[lastIndex - 1].article_image,
+			date: Posts.posts[lastIndex - 1].date,
 		});
 	}, []);
 
 	return (
 		<>
-			<h1>Blogs</h1>
+			<h1>LATE START DEV</h1>
 			<div className={styles.latestCont}>
 				<div className={styles.latestImg}>
 					<Image
@@ -33,16 +34,40 @@ const Display = () => {
 						layout='responsive'
 					/>
 				</div>
-				<div>
+				<div className={styles.latestContent}>
 					<h2>{lastPost.title}</h2>
 					<p>{lastPost.description}</p>
+					<p className={styles.latestDate}>{lastPost.date}</p>
 				</div>
 			</div>
-			<div>
+			<h2 style={{paddingTop: "10px"}}>Featured Articles</h2>
+			<div className={styles.featuredContain}>
 				{Posts.posts.map((post) => {
-					return <p key={post.id}>{post.title}</p>;
+					return (
+						post.featured && (
+							<div className={styles.featureItem}>
+								<div className=''>
+									<Image
+										src={post.article_image}
+										width={1200}
+										height={630}
+										alt={post.title}
+										priority={true}
+										layout='responsive'
+									/>
+								</div>
+								<div className={styles.featuredTitle}>
+									<h4>{post.title}</h4>
+									<p className={styles.latestDate}>{post.date}</p>
+								</div>
+							</div>
+						)
+					);
 				})}
 			</div>
+			{Posts.posts.map((post) => {
+				return <p key={post.id}>{post.title}</p>;
+			})}
 		</>
 	);
 };
