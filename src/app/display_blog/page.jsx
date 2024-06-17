28b4fa6cae30052b8913f8dd97467d7f0dd573f8
egnica <46502133+egnica@users.noreply.github.com>
2024-06-16 23:42:31 -0500

    commit

--- src/app/display_blog/page.jsx
+++ src/app/display_blog/page.jsx
@@ -1,21 +1,48 @@
 "use client";
 import styles from "./page.module.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import Fade from "@/components/Fade";
 import Posts from "@/app/posts.json";
+import Image from "next/image";
+import Link from "next/link";
 
 const Display = () => {
-	const [fade, setFade] = useState(false);
+	const [lastPost, setLastPost] = useState([]);
 
-	const [newState, setNewState] = useState(false);
+	useEffect(() => {
+		const lastIndex = Posts.posts.length;
 
-	const clickHandler = () => {
-		setFade(!fade);
-	};
-
+		setLastPost({
+			title: Posts.posts[lastIndex - 1].title,
+			description: Posts.posts[lastIndex - 1].description,
+			articleImage: Posts.posts[lastIndex - 1].article_image,
+		});
+	}, []);
+	console.log(lastPost);
 	return (
 		<>
 			<h1>Blogs</h1>
+			<div className={styles.latestCont}>
+				<div className={styles.latestImg}>
+					<Image
+						src={lastPost.articleImage}
+						width={1200}
+						height={630}
+						alt={lastPost.title}
+						priority={true}
+						layout='responsive'
+					/>
+				</div>
+				<div>
+					<h2>{lastPost.title}</h2>
+					<p>{lastPost.description}</p>
+				</div>
+			</div>
+			<div>
+				{Posts.posts.map((post) => {
+					return <p key={post.id}>{post.title}</p>;
+				})}
+			</div>
 		</>
 	);
 };
