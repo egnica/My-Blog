42cbc8aa0c9140f32961fc0b21d467779ffa26bd
egnica <46502133+egnica@users.noreply.github.com>
2024-06-22 20:19:47 -0500

    commit

--- src/app/blog-post/page.jsx
+++ src/app/blog-post/page.jsx
@@ -1,17 +1,21 @@
 "use client";
 import {useSearchParams} from "next/navigation";
 import Posts from "../posts.json";
+import {Suspense} from "react";
 
 export default function Blog_Post() {
 	const searchParams = useSearchParams();
 	const queryName = searchParams.get("article");
+
 	const post = Posts.posts.filter((post) => post.query === queryName);
 
 	return (
 		<>
-			<h1>{post[0].title}</h1>
-			<p>test test</p>
-			{/* <p dangerouslySetInnerHTML={{__html: person.bio}} /> */}
+			<Suspense fallback={<div>Loading...</div>}>
+				<h1>{post[0].title}</h1>
+				<p>test test</p>
+				{/* <p dangerouslySetInnerHTML={{__html: person.bio}} /> */}
+			</Suspense>
 		</>
 	);
 }
