f3cb7c9c3c8ec795213fb1563e2a439847b7addd
egnica <46502133+egnica@users.noreply.github.com>
2024-06-26 14:23:51 -0500

    added dangerouslySetInnerHTML

--- src/app/posts/[query]/page.jsx
+++ src/app/posts/[query]/page.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import Image from "next/image";
 import Posts from "../../posts.json";
+import styles from "./page.module.css";
 
-const BlogPost = (prams) => {
-	const queryString = prams.params.query;
+const BlogPost = (params) => {
+	const queryString = params.params.query;
 	const foundPost = Posts.posts.find((item) => item.query === queryString);
+
 	return (
 		<>
 			<h1>{foundPost.title}</h1>
-			<p>{foundPost.body_1}</p>
+			<div className={styles.featuredImg}>
+				<Image
+					src={foundPost.article_image}
+					width={869}
+					height={400}
+					alt={foundPost.title}
+					priority={true}
+					layout='responsive'
+				/>
+			</div>
+			<div className={styles.content_container}>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: foundPost.body_1,
+					}}
+				></div>
+			</div>
 		</>
 	);
 };
