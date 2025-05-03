97e05ab7f74761ba35c55e3286f0a53392f0035c
egnica <46502133+egnica@users.noreply.github.com>
2025-05-03 18:15:35 -0500

    static pages add to sitemap

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -3,21 +3,41 @@ import Posts from "../posts.json";
 export async function GET() {
   const baseUrl = "https://latestartdev.com";
 
-  const urls = Posts.posts.map((post) => {
-    return `
-        <url>
-          <loc>${baseUrl}/posts/${post.query}</loc>
-          <lastmod>${new Date(
-            post.modified_time || post.date
-          ).toISOString()}</lastmod>
-        </url>`;
-  });
+  const staticUrls = [
+    {
+      loc: `${baseUrl}/`,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      loc: `${baseUrl}/posts`,
+      lastmod: new Date().toISOString(),
+    },
+    {
+      loc: `${baseUrl}/posts/archive`,
+      lastmod: new Date().toISOString(),
+    },
+  ];
+
+  const postUrls = Posts.posts.map((post) => ({
+    loc: `${baseUrl}/posts/${post.query}`,
+    lastmod: new Date(post.modified_time || post.date).toISOString(),
+  }));
+
+  const urls = [...staticUrls, ...postUrls]
+    .map(
+      (url) => `
+    <url>
+      <loc>${url.loc}</loc>
+      <lastmod>${url.lastmod}</lastmod>
+    </url>`
+    )
+    .join("\n");
 
   const xml = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${urls.join("\n")}
-      </urlset>`;
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+    </urlset>`;
 
   return new Response(xml.trim(), {
     headers: {
