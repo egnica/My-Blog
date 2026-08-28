52ea92d661b4611fa32a1177b60085698781317f
Nicholas Egner <nictacks@gmail.com>
2026-08-28 12:15:00 -0500

    Use shared JSON in blog archive

--- src/app/posts/archive/page.jsx
+++ src/app/posts/archive/page.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import Posts from "../../posts.json";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../page.module.css";
+import {
+  BLOG_REVALIDATE_SECONDS,
+  getBlogPosts,
+} from "../../../lib/blogData";
 
-function page() {
-  const PostsObject = Posts.posts.map;
+export const revalidate = BLOG_REVALIDATE_SECONDS;
+
+export default async function ArchivePage() {
+  const posts = await getBlogPosts();
 
   return (
     <div>
       <h1>Post Archive</h1>
-      <div className={styles.postWrapper}>
-        {Posts.posts.map((post, index) => {
-          return (
-            <Link key={index} href={post.query}>
-              <div className={styles.articleRow}>
-                <Image
-                  style={{ paddingRight: "10px" }}
-                  src={post.article_image}
-                  height={100}
-                  width={200}
-                  alt={post.title}
-                />
-                <div className={styles.textWrap}>
-                  <p>
-                    {" "}
-                    <em>{post.date}</em>
-                  </p>
 
-                  <h2>{post.title}</h2>
-                </div>
+      <div className={styles.postWrapper}>
+        {[...posts].reverse().map((post) => (
+          <Link key={post.id || post.slug} href={`/posts/${post.slug}`}>
+            <div className={styles.articleRow}>
+              <Image
+                style={{ paddingRight: "10px" }}
+                src={post.hero_image}
+                height={100}
+                width={200}
+                alt={post.title}
+              />
+
+              <div className={styles.textWrap}>
+                <p>
+                  <em>{post.date}</em>
+                </p>
+                <h2>{post.title}</h2>
               </div>
-            </Link>
-          );
-        })}
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
 }
-
-export default page;
