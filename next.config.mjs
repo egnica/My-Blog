e5e27ad8e5eb86924a54c576848aa0f471f1ff2c
Nicholas Egner <nictacks@gmail.com>
2026-08-28 12:16:36 -0500

    Allow shared blog image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: [
-			"latestartbucket.s3.us-east-2.amazonaws.com",
-			"images.squarespace-cdn.com",
-		],
-	},
+  images: {
+    domains: [
+      "latestartbucket.s3.us-east-2.amazonaws.com",
+      "nciholasegner.s3.us-east-2.amazonaws.com",
+      "images.squarespace-cdn.com",
+    ],
+  },
 };
 
 export default nextConfig;
