12e9e31d6c89928625f66bd79e7b031af7416366
Nicholas Egner <nictacks@gmail.com>
2026-08-28 12:17:10 -0500

    Use static revalidation value

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,10 +1,7 @@
 import PostsDisplay from "../../components/PostsDisplay";
-import {
-  BLOG_REVALIDATE_SECONDS,
-  getBlogPosts,
-} from "../../lib/blogData";
+import { getBlogPosts } from "../../lib/blogData";
 
-export const revalidate = BLOG_REVALIDATE_SECONDS;
+export const revalidate = 60;
 
 export default async function PostsPage() {
   const posts = await getBlogPosts();
